3452d22d466f679cc8d7ad717f80bf56c5714515
Henry <hsilva4495@eagle.fgcu.edu>
2017-12-09 23:08:04 -0500

    look at this shit Toni fang

--- Student Housing GUI/src/LocationPage.java
+++ Student Housing GUI/src/LocationPage.java
@@ -1,8 +1,14 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Window;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 
 import javax.swing.*;
-
 
 public class LocationPage extends MainScreen{
 	
@@ -10,7 +16,7 @@ public class LocationPage extends MainScreen{
 
 
 	
-	public LocationPage(JFrame window){
+	public LocationPage(JFrame window) {
 		super(window);
 		this.remove(BwhiteBox);
 		this.remove(blueBackground);
@@ -23,16 +29,56 @@ public class LocationPage extends MainScreen{
 		locationPane.setBounds(260, 70, 730, 570);
 		add(locationPane);
 
+
 		
-		blueBackground = new JLabel("");
-		blueBackground.setBounds(-30, -0, 1034, 683);
-		blueBackground.setIcon(new ImageIcon("Images/BlueBG.png"));
-		this.add(blueBackground);
 		
-		revalidate();
+		
+		try {
+			String latitude = "40.714728";
+			String longitude = "-73.998672";
+			String imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="
+			+ latitude
+			+ ","
+			+ longitude
+			+ "&zoom=11&size=612x612&scale=2&maptype=roadmap";
+			String destinationFile = "image.jpg";
+			
+			// read the map image from Google
+			// then save it to a local file: image.jpg
+		
+			URL url = new URL(imageUrl);
+			InputStream is = url.openStream();
+			OutputStream os = new FileOutputStream(destinationFile);
+			
+			byte[] b = new byte[2048];
+			int length;
+			
+			while ((length = is.read(b)) != -1) {
+			os.write(b, 0, length);
+			}
+			
+			is.close();
+			os.close();
+			} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+			System.exit(1);
+			}
+			// create a GUI component that loads the image: image.jpg
+			//
+			ImageIcon imageIcon = new ImageIcon((new ImageIcon("image.jpg")).getImage().getScaledInstance(630, 600, java.awt.Image.SCALE_SMOOTH));
+			locationPane.add(new JLabel(imageIcon));
+		
+			blueBackground = new JLabel("");
+			blueBackground.setBounds(-30, -0, 1034, 683);
+			blueBackground.setIcon(new ImageIcon("Images/BlueBG3"));
+			
+			add(blueBackground);
 	}
 
 	
 }
+
 
 
