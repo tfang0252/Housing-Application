311ec43f19bfec9ee6b2a3d5a4f4f2e9c0d700e5
tfang0252 <32147688+tfang0252@users.noreply.github.com>
2017-12-10 02:06:49 -0500

    hell ya for realz

--- Student Housing GUI/src/LocationPage.java
+++ Student Housing GUI/src/LocationPage.java
@@ -1,7 +1,10 @@
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Window;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,119 +13,79 @@ import java.net.URL;
 
 import javax.swing.*;
 
-public class LocationPage extends MainScreen{
+public class LocationPage extends MainScreen implements ActionListener{
 	
 	private JPanel locationPane;
-<<<<<<< HEAD
-	private int zoom =10;
 	private JLabel testMap;
-=======
-
-
->>>>>>> parent of 0d3f0aa... map stuff
-	
+	private JButton zoomIn;
+	private JButton zoomOut;
+	private int i = 3;
 	public LocationPage(JFrame window) {
 		super(window);
 		this.remove(BwhiteBox);
 		this.remove(blueBackground);
 		
-<<<<<<< HEAD
+		zoomIn = new JButton("+");
+		zoomOut = new JButton("-");
+		zoomIn.addActionListener(this);
+		zoomOut.addActionListener(this);
+	
+		zoomIn.setBounds(925,325,50,20);
+		add(zoomIn);
+		zoomOut.setBounds(925,350,50,20);
+		add(zoomOut);
+
 	
 		testMap = new JLabel("");
-		testMap.setBounds(260, 70, 730, 570);
-=======
-		
->>>>>>> parent of 0d3f0aa... map stuff
+		testMap.setBounds(360, 70, 1034, 683);
+
+		testMap.setIcon(new ImageIcon("Images/map3.png"));
 		
 		
 		locationPane = new JPanel();
 		locationPane.setForeground(Color.WHITE);
-		locationPane.setBounds(260, 70, 730, 570);
-<<<<<<< HEAD
+		locationPane.setBounds(270, 70, 730, 570);
+
 		locationPane.add(testMap);
-		locationPane.addMouseWheelListener(new MouseWheelListener(){
-			@Override
-			public void mouseWheelMoved(MouseWheelEvent e) {
-				//scroll down
-				if(e.getWheelRotation() > 0) {
-					zoom--;
-					getMap(zoom);
-					System.out.println(zoom);
-				}
-				//scroll up
-				if(e.getWheelRotation() < 0) {
-					zoom++;
-					getMap(zoom);
-					System.out.println(zoom);
-					
-				}
-				
-			}
-			
-		});
-		getMap(zoom);
-		add(locationPane);
 		
-			
 	
-			// create a GUI component that loads the image: image.jpg
-			//
-	
-=======
-		add(locationPane);
-		
->>>>>>> parent of 0d3f0aa... map stuff
-		
-		
-		try {
-			String latitude = "40.714728";
-			String longitude = "-73.998672";
-			String imageUrl = "https://maps.googleapis.com/maps/api/staticmap?center="
-			+ latitude
-			+ ","
-			+ longitude
-			+ "&zoom=11&size=612x612&scale=2&maptype=roadmap";
-			String destinationFile = "imageMap1.jpg";
-			
-			System.out.println(imageUrl);
-			// read the map image from Google
-			// then save it to a local file: image.jpg
-		
-			URL url = new URL(imageUrl);
-			System.out.println(url);
-			InputStream is = url.openStream();
-			OutputStream os = new FileOutputStream(destinationFile);
-			
-			byte[] b = new byte[2048];
-			int length;
-			
-			while ((length = is.read(b)) != -1) {
-			os.write(b, 0, length);
-			}
-			
-			length++;
-			
-			is.close();
-			os.close();
-			
-			
-			testMap.setIcon(new ImageIcon(destinationFile));
-			revalidate();
-			} catch (IOException e) {
-			
-			e.printStackTrace();
-			
-			System.exit(1);
-			}
-			// create a GUI component that loads the image: image.jpg
-			//
-			ImageIcon imageIcon = new ImageIcon((new ImageIcon("imageMap1.jpg")).getImage().getScaledInstance(630, 600, java.awt.Image.SCALE_SMOOTH));
-			locationPane.add(new JLabel(imageIcon));
+		add(testMap);
+
+			this.add(BwhiteBox);
 		
 			blueBackground = new JLabel("");
 			blueBackground.setBounds(-30, -0, 1034, 683);
 			blueBackground.setIcon(new ImageIcon("Images/BlueBG3.png"));
 			add(blueBackground);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+		if(e.getSource() == zoomIn) {
+			i++;
+			
+			
+			if(i>5 || i <1) {
+				i=3;
+			}
+			System.out.println(i);
+			testMap.setIcon(new ImageIcon("Images/map"+i+".png"));
+			revalidate();
+		}
+		if(e.getSource() == zoomOut) {
+			i--;
+			
+			
+			if(i>5 || i <1) {
+				i=3;
+			}
+			System.out.println(i);
+			testMap.setIcon(new ImageIcon("Images/map"+i+".png"));
+			revalidate();
+		}
+		
 	}
 
 	
